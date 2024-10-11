c25a3e15c5690efda7d5342e68e2f223b79c93b0
Muhammad-Nastain-0020 <harmonicelila@gmail.com>
2024-10-11 19:22:14 +0700

    Delete 23.240.0020_Muhammad Nastain_Pertemuan_2 directory

--- 23.240.0020_Muhammad Nastain_Pertemuan_2/Latihan/Web-oop/NameSpace.php
+++
@@ -1,6 +0,0 @@
-<?php 
-require_once "conflict.php";
-// Membuat Objek dari kelas conflict
-$conflic1 = new \Data\One\conflict();
-$conflic2 = new \Data\Two\conflict();
-?>
--- 23.240.0020_Muhammad Nastain_Pertemuan_2/Latihan/Web-oop/ObjekCar.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require_once "Car.php";
-
-$car = new Avanza();
-echo $car->drive();
-echo $car->isMaintance();
-echo $car->getBrand();
-
-?>
